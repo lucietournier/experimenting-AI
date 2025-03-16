388e21c711bb351b080f872bc409cde23ba12cf0
lucietournier <lucie.mc.tournier@gmail.com>
2025-03-16 12:08:08 +0000

    improved application

--- flower_classifier/app.py
+++ flower_classifier/app.py
@@ -1,7 +1,7 @@
 from fastai.vision.all import * 
 import gradio as gr
 
-learner = load_learner('flower_classifier/models/first_classifier.pkl')
+learner = load_learner('models/first_classifier.pkl')
 
 categories = learner.dls.vocab
 
@@ -9,9 +9,14 @@ def classify_image(img) :
     pred, idx, probs = learner.predict(img)
     return(dict(zip(categories, map(float, probs))))
 
+title = "My first flower classifier"
+description = "This classifier can handle 5 types of flowers : primroses, roses, tulips, orchids and geraniums."
 image = gr.Image()
 label = gr.Label()
-examples = ['primrose.lpg']
+examples = ['primrose.jpg']
 
-intf = gr.Interface(fn=classify_image, inputs=image, outputs=label, examples=examples)
-intf.launch(inline=False)
+intf = gr.Interface(fn=classify_image, 
+                    title = title, description = description,
+                    inputs=image, outputs=label, 
+                    examples=examples)
+intf.launch(share=True)
